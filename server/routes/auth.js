90f1f90c6f41ffbdabb32479aba1d4a7dfe44f91
Duncan Moyo <duncanfmoyo@gmail.com>
2021-02-08 17:47:12 +0200

    successfully copied from repository ecommerceMERN

---
+++ server/routes/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+//import
+const { createOrUpdateUser } = require("../controllers/auth");
+
+// route
+router.get("/create-or-update-user", createOrUpdateUser);
+
+module.exports = router;
---
+++ server/routes/user.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+// route
+router.get("/user", (req, res) => {
+  res.json({
+    data: "Hey you hit user API endpoint",
+  });
+});
+
+module.exports = router;
